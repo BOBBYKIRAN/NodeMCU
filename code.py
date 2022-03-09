b5a7da0822a9bb855bfb33a2de70ccc0f45f8d31
BOBBYKIRAN <101236759+BOBBYKIRAN@users.noreply.github.com>
2022-03-09 11:09:04 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
-#Access point setup:
-http://www.martyncurrey.com/esp8266-and-the-arduino-ide-part-2-control-an-led-from-a-web-page-using-access-point-mode-ap/
-  
+#Access point setup
+import numpy as np
+https://www.martyncurrey.com/esp8266-and-the-arduino-ide-part-2-control-an-led-from-a-web-page-using-access-point-mode-ap/
+
   
