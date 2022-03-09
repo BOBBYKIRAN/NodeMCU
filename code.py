38dc1515ebf5cf8b5ad60083a4c59cc41df11c6a
BOBBYKIRAN <101236759+BOBBYKIRAN@users.noreply.github.com>
2022-03-09 12:26:46 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,13 @@
-#Access point setup
+
+TOPIC 1:
+#NODE MCU Access point setup
 import numpy as np
 https://www.martyncurrey.com/esp8266-and-the-arduino-ide-part-2-control-an-led-from-a-web-page-using-access-point-mode-ap/
+  OR
+https://randomnerdtutorials.com/esp8266-nodemcu-access-point-ap-web-server/
 
+TOPIC 2:  
+#NODEMCU MQTT PROTOCOL OPERATIONS EXAMPLE:
+https://www.instructables.com/NodeMCU-MQTT-Basic-Example/
   
+TOPIC 3:
